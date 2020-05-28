aa2ab2890b15d6fb03ca4560c4c7a0f969458550
Mechanize <mechanize100@gmail.com>
2020-05-28 18:45:43 -0400

    update script

--- create_npm_and_yarn.sh
+++ create_npm_and_yarn.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
 
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update
-sudo apt install yarn
-node --version
-npm --version
-sudo apt-get install nodejs
-sudo apt-get install npm
+sudo apt install -y yarn nodejs
 sudo npm install --global gulp-cli
-cd jellyfin-web/
 yarn install
 yarn serve
 #yarn build:developmnet
